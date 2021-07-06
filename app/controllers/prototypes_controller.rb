7b1ac504e03cc0fe4921d2b56e3cba537970bd0a
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-07-06 15:17:05 +0900

    編集機能実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,24 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    
+  end
+
   private
 
   def prototype_params
