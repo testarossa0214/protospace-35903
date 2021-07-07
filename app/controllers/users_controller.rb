1546dc094ba2182fb434e80fbf255dd81e60212d
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-07-07 17:14:54 +0900

    ユーザー詳細ページ実装
    コメント作成が編集完了になってしまっている

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user.save
   end
 
+  def show
+    @user = User.find(params[:id])
+    @prototypes = Prototype.includes(:user)
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :profile, :occupation, :position)
