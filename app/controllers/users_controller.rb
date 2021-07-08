d67ea3ea017b9e7ffc6c76e19b39599474998c33
testarossa0214 <85383842+testarossa0214@users.noreply.github.com>
2021-07-08 12:26:29 +0900

    ユーザーのアクセス範囲の制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = Prototype.includes(:user)
+    @prototypes = @user.prototypes
   end
 
   private
